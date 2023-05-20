906f8ba6b749c69e972cbc7dfb7c53ed4d2ea7ac
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2023-05-20 15:32:10 +0200

    Implement SimpleClient

--- KVStore/clients/clients.py
+++ KVStore/clients/clients.py
@@ -1,6 +1,10 @@
+from __future__ import annotations
+
 from typing import Union, Dict
 import grpc
 import logging
+
+from KVStore.kvstorage.kvstorage import KVStorageService
 from KVStore.protos.kv_store_pb2 import GetRequest, PutRequest, GetResponse
 from KVStore.protos.kv_store_pb2_grpc import KVStoreStub
 from KVStore.protos.kv_store_shardmaster_pb2 import QueryRequest, QueryResponse, QueryReplicaRequest, Operation
@@ -15,36 +19,43 @@ def _get_return(ret: GetResponse) -> Union[str, None]:
     else:
         return None
 
-class SimpleClient:
+
+class SimpleClient(KVStorageService):
+
+    """
+    Simple client for key-value storage service
+    """
     def __init__(self, kvstore_address: str):
+        super().__init__()
         self.channel = grpc.insecure_channel(kvstore_address)
         self.stub = KVStoreStub(self.channel)
 
-    def get(self, key: int) -> Union[str, None]:
-        """
-        To fill with your code
-        """
+    def get(self, key: int) -> str | None:
+        return _get_return(self.stub.Get(GetRequest(key=key)))
 
+    def l_pop(self, key: int) -> str | None:
+        return _get_return(self.stub.LPop(GetRequest(key=key)))
 
-    def l_pop(self, key: int) -> Union[str, None]:
-        """
-        To fill with your code
-        """
-
-    def r_pop(self, key: int) -> Union[str, None]:
-        """
-        To fill with your code
-        """
+    def r_pop(self, key: int) -> str | None:
+        return _get_return(self.stub.RPop(GetRequest(key=key)))
 
     def put(self, key: int, value: str):
-        """
-        To fill with your code
-        """
+        self.stub.Put(PutRequest(key=key, value=value))
 
     def append(self, key: int, value: str):
-        """
-        To fill with your code
-        """
+        self.stub.Append(PutRequest(key=key, value=value))
+
+    def redistribute(self, destination_server: str, lower_val: int, upper_val: int):
+        raise NotImplementedError
+
+    def transfer(self, keys_values: list):
+        raise NotImplementedError
+
+    def add_replica(self, server: str):
+        raise NotImplementedError
+
+    def remove_replica(self, server: str):
+        raise NotImplementedError
 
     def stop(self):
         self.channel.close()
@@ -68,18 +79,15 @@ class ShardClient(SimpleClient):
         To fill with your code
         """
 
-
     def r_pop(self, key: int) -> Union[str, None]:
         """
         To fill with your code
         """
 
-
     def put(self, key: int, value: str):
         """
         To fill with your code
         """
-
 
     def append(self, key: int, value: str):
         """
@@ -99,21 +107,17 @@ class ShardReplicaClient(ShardClient):
         To fill with your code
         """
 
-
     def r_pop(self, key: int) -> Union[str, None]:
         """
         To fill with your code
         """
-
 
     def put(self, key: int, value: str):
         """
         To fill with your code
         """
 
-
     def append(self, key: int, value: str):
         """
         To fill with your code
         """
-
